087353ccb81a7e8fd9c1bbf23f1bb638f248fc7b
fabiobiondi <info@fabiobiondi.com>
2025-02-02 00:53:45 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+import { withZephyr } from 'vite-plugin-zephyr';
+
+// https://vitejs.dev/config/
+export default defineConfig(() => ({
+	plugins: [
+		angular(),
+		withZephyr()
+	],
+}));
